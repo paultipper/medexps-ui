c890d99996b3b891b0e3aac109aece5f690e14e2
Paul Tipper <ptipper@gmail.com>
2021-08-10 17:33:15 +0100

    Initial commit

---
+++ src/components/DashboardNavbar.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  AppBar,
+  Badge,
+  Box,
+  Hidden,
+  IconButton,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
+import InputIcon from '@material-ui/icons/Input';
+import { makeStyles } from '@material-ui/core/styles';
+import Logo from './Logo';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
+  const classes = useStyles();
+  const [notifications] = useState([]);
+
+  return (
+    <AppBar elevation={0} {...rest}>
+      <Toolbar>
+        <RouterLink to="/">
+          <Logo />
+        </RouterLink>
+        <Box sx={{ flexGrow: 1 }} />
+        <Typography variant="h2" className={classes.title}>
+          Medical Expenses
+        </Typography>
+        <Hidden lgDown>
+          <IconButton color="inherit">
+            <Badge
+              badgeContent={notifications.length}
+              color="primary"
+              variant="dot"
+            >
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton color="inherit">
+            <InputIcon />
+          </IconButton>
+        </Hidden>
+        <Hidden lgUp>
+          <IconButton color="inherit" onClick={onMobileNavOpen}>
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+DashboardNavbar.propTypes = {
+  onMobileNavOpen: PropTypes.func
+};
+
+export default DashboardNavbar;
