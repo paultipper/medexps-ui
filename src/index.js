81a0fe3895e372053430f5f076c3d8b9f82a093a
Paul Tipper <ptipper@gmail.com>
2021-08-10 18:02:49 +0100

    Install amplify and deployed to AWS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+
+Amplify.configure(awsExports);
 
 ReactDOM.render((
   <BrowserRouter>
